f43ee7a47d9a6b4b8378186d8a4130e4a37ef442
durga6424 <durga6424@LAPTOP-KRCD92MG>
2023-03-30 18:29:16 +0530

    c10

--- Class1.cs
+++ Class1.cs
@@ -1,8 +1,16 @@
 using System;
+using System.IO.Compression;
+
 
 public class ExampleController 
 {
   private static string TargetDirectory = "/example/directory/";
 
-}
+  public void ExtractEntry(IEnumerator<ZipArchiveEntry> entriesEnumerator)
+  {
+    ZipArchiveEntry entry = entriesEnumerator.Current;
+    string destinationPath = Path.Combine(TargetDirectory, entry.FullName);
 
+    entry.ExtractToFile(destinationPath);
+  }
+}
