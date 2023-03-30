eb2a79bdafa193cb24110e56aeb73ef0e5632d46
durga6424 <durga6424@LAPTOP-KRCD92MG>
2023-03-30 18:13:44 +0530

    c8

--- Class1.cs
+++ Class1.cs
@@ -1,16 +1,8 @@
 using System;
 
-partial class Program
+public class ExampleController 
 {
-  static void Main(string[] args)
-  {
-    Console.Write("Please enter your name: ");
-    string name = Console.ReadLine();
+  private static string TargetDirectory = "/example/directory/";
 
-    Console.WriteLine("Hello, " + name + "!");
-
-    Console.WriteLine("Press any key to exit...");
-    Console.ReadKey();
-  }
 }
 
