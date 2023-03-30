1907d86cfdb497761535d6fba9ff8b6f6f42da16
JayasreeAkul <124138982+JayasreeAkul@users.noreply.github.com>
2023-03-30 15:51:59 +0530

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -1,39 +1,15 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Data;
-using OWASP.WebGoat.NET.App_Code.DB;
-using OWASP.WebGoat.NET.App_Code;
 
-namespace OWASP.WebGoat.NET
+class Program
 {
-	public partial class ReflectedXSS : System.Web.UI.Page
-	{
-        private IDbProvider du = Settings.CurrentDbProvider;
-        
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            if (Request["city"] != null)
-                LoadCity(Request["city"]);
-        }
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
 
-		void LoadCity (String city)
-		{
-            DataSet ds = du.GetOffice(city);
-            lblOutput.Text = "Here are the details for our " + city + " Office";
-            dtlView.DataSource = ds.Tables[0];
-            dtlView.DataBind();
-		}
+        Console.WriteLine("Hello, " + name + "!");
 
-        void FixedLoadCity (String city)
-        {
-            DataSet ds = du.GetOffice(city);
-            lblOutput.Text = "Here are the details for our " + Server.HtmlEncode(city) + " Office";
-            dtlView.DataSource = ds.Tables[0];
-            dtlView.DataBind();
-        }
-	}
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
 }
