9cc0e263037a9762d16e4d39282cbbe58f71a5c4
durga6424 <durga6424@LAPTOP-KRCD92MG>
2024-11-21 19:06:10 +0530

    chat gpt commit

--- Class1.cs
+++ Class1.cs
@@ -1,22 +1,33 @@
 using System;
 using System.IO.Compression;
 using System.Linq;
-
-public class ExampleController 
-using System.Threading.Tasks;
-  private static string TargetDirectory = "/example/directory/";
-
-  public void ExtractEntry(IEnumerator<ZipArchiveEntry> entriesEnumerator)
 using System.Text;
-    ZipArchiveEntry entry = entriesEnumerator.Current;
-    string destinationPath = Path.Combine(TargetDirectory, entry.FullName);
-
-    entry.ExtractToFile(destinationPath);
 using System.Threading.Tasks;
 
 namespace console_project_example
 {
+  public class ExampleController
+  {
+    private static string TargetDirectory = "/example/directory/";
+
+    public void ExtractEntry(IEnumerator<ZipArchiveEntry> entriesEnumerator)
+    {
+      ZipArchiveEntry entry = entriesEnumerator.Current;
+      string destinationPath = Path.Combine(TargetDirectory, entry.FullName);
+
+      // Ensure the directory exists before extracting the file
+      string directoryPath = Path.GetDirectoryName(destinationPath);
+      if (!Directory.Exists(directoryPath))
+      {
+        Directory.CreateDirectory(directoryPath);
+      }
+
+      entry.ExtractToFile(destinationPath);
+    }
+  }
+
   internal class Class1
   {
+    // Class1 can remain empty or you can add functionality here
   }
 }
